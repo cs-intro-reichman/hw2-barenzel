71a0ddc05a129fd3ad220254a4a07ce1ec636640
barenzel <bar.enzel@post.runi.ac.il>
2024-11-14 21:12:27 +0200

    1 and 2 completed

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,15 +4,15 @@ public class TimeCalc {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int minutesToAdd = Integer.parseInt(args[1]);
 
-        int totalMinutes = (hours*60)+minutes+minutesToAdd;
-        int totalHours = totalMinutes/60;
-        int newMinutes = totalMinutes-(totalHours*60);
-        int newHours = totalHours%24;
+        int totalMinutes = (hours * 60) + minutes+minutesToAdd;
+        int totalHours = totalMinutes / 60;
+        int newMinutes = totalMinutes - (totalHours*60);
+        int newHours = totalHours % 24;
 
-        String finalHours = (newHours<10) ? "0"+newHours : ""+newHours;
-        String finalMinutes = (newMinutes<10) ? "0"+newMinutes : ""+newMinutes;
+        String finalHours = (newHours < 10) ? "0" + newHours : "" + newHours;
+        String finalMinutes = (newMinutes < 10) ? "0" + newMinutes : "" + newMinutes;
 
-        System.out.println(finalHours+":"+finalMinutes);
+        System.out.println (finalHours + ":" + finalMinutes);
      }
         
     
