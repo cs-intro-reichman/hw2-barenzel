12246c2bd1bdccf1479785d66fd8bd72a55dd9f4
barenzel <bar.enzel@post.runi.ac.il>
2024-11-15 17:12:24 +0200

    1,2, and 3 completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		boolean isPrime = num > 1;
+		double calcPi = 0.0;
+		for(int i = 2; i < num && isPrime; i++) {
+			if (isPrime) {
+				calcPi += (1 / (double)num);
+			} 	
+		}
+        System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi approximated: " + calcPi);
+
 	}
 }
