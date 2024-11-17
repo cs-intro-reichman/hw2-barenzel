d55b557320f738048bffec92b7db872fa0885bd1
barenzel <bar.enzel@post.runi.ac.il>
2024-11-17 22:33:59 +0200

    completed

--- CalcPi.java
+++ CalcPi.java
@@ -4,8 +4,8 @@ public class CalcPi {
 		int numberOfelements = Integer.parseInt(args[0]);
 		double calcPi = 1.0;
 		double one = -1;
-		double num = 2;
-		for (int i = 1; i <=  numberOfelements; num++) { 
+		double num = 3;
+		for (int i = 1; i < numberOfelements; num += 2) { 
 			if (num % 2 != 0) {
 				calcPi += one * (1 / (double)num);
 				one *= -1;
