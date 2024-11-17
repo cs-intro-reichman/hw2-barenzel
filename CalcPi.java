747fbbb8609ef704f8effbac47b4b7687ccf39ff
barenzel <bar.enzel@post.runi.ac.il>
2024-11-17 22:42:29 +0200

    completed

--- CalcPi.java
+++ CalcPi.java
@@ -14,27 +14,9 @@ public class CalcPi {
 				continue;
 			}
 		}	
+		calcPi *= 4;
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi approximated: " + calcPi*4);
-		
-
-		/* 
-		int num = Integer.parseInt(args[0]);
-		double calcPi = 1.0;
-		double one = -1;
-		for (int i = 2; i <= num; i++) {
-			int odd = i;
-				if (odd % 2 == 0){
-					continue;
-				} else {
-                    calcPi += one * (1 / (double)odd);
-					one *= -1;
-				}
-		}
-			
-		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi approximated: " + calcPi*4);
-		*/
+		System.out.println("pi approximated: " + calcPi);
 		}
 
 }
