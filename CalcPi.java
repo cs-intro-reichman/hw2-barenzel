da8f6a40088de56022cadcccf5771fbe2fddf801
barenzel <bar.enzel@post.runi.ac.il>
2024-11-17 21:57:20 +0200

    completed

--- CalcPi.java
+++ CalcPi.java
@@ -2,22 +2,23 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		int num = Integer.parseInt(args[0]);
-		boolean isPrime = num > 1;
 		double calcPi = 1.0;
-		for (int i = 3; i < num && isPrime; i++) {
-			for (int j = 2; j < i; j++) {
-			if (i % j == 0) {
-		    continue;
-			} else if (j % 2 == 0) {
-				calcPi -= (1.0 / (double)i);
-			} else if (j % 2 != 0) {
-				calcPi += (1.0 / (double)i);
-			}
-		}	
+		double one = -1;
+		for (int i = 2; i <= num; i++) {
+			int odd = i;
+				if (odd % 2 == 0){
+					continue;
+				} else {
+                    calcPi += one * (1 / (double)odd);
+					one *= -1;
+				}
+		}
+			
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi approximated: " + calcPi*4);
 		}
 
-        System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi approximated: " + calcPi*4);
-
-	}
-} 
+}
+		
+		
+	
